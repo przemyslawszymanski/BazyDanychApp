83bd254594a4da7f90a621cc1bf8a24ff28c5cce
przemyslawszymanski <szymanski.przemyslaw.ps@gmail.com>
2018-12-19 22:25:49 +0100

    zaawansowane zmiany

--- WpfApp1/WpfApp1/MainWindow.xaml.cs
+++ WpfApp1/WpfApp1/MainWindow.xaml.cs
@@ -23,18 +23,17 @@ namespace WpfApp1
     {
         LINQtoSQLClassDataContext dc = new LINQtoSQLClassDataContext(Properties.Settings.Default.BibliotekaDBConnectionString);
         int index_users;
-        int index_list;
         int index_return;
+        int index_books;
 
         public MainWindow()
         {
             InitializeComponent();
 
-            if (dc.DatabaseExists()) List_Users.ItemsSource = dc.Czytelnik;
+            var users_list = from czytelnik in dc.Czytelnik
+                             select new { czytelnik.Imie, czytelnik.Naziwsko, czytelnik.Email, czytelnik.Telefon, czytelnik.Adres };
+            if (dc.DatabaseExists()) List_Users.ItemsSource = users_list;
 
-            
-
-            
 
             if (dc.DatabaseExists()) Add_Book.ItemsSource = dc.Ksiazka;
 
@@ -62,6 +61,12 @@ namespace WpfApp1
             int index = ListViewMenu.SelectedIndex;
             if (index == 0)
             {
+                var books_list = from ksiazka in dc.Ksiazka
+                                 join autor in dc.Autor on ksiazka.Fk_Author_Id equals autor.Pk_Autor_Id
+                                 join kategoria in dc.Kategoria on ksiazka.Fk_Kategoria_Id equals kategoria.Pk_Kategoria_Id
+                                 select new { ksiazka.Tytul, autor.Imie, autor.Nazwisko, kategoria.Kategoria1, ksiazka.Wydawnictwo, ksiazka.Dostepnosc };
+                if (dc.DatabaseExists()) List_Book.ItemsSource = books_list;
+
                 BookList.Visibility = Visibility.Visible;
                 RentBook.Visibility = Visibility.Collapsed;
                 AddBook.Visibility = Visibility.Collapsed;
@@ -83,6 +88,10 @@ namespace WpfApp1
 
         private void ButtonUser_Click(object sender, RoutedEventArgs e)
         {
+            var users_list = from czytelnik in dc.Czytelnik
+                             select new {czytelnik.Imie, czytelnik.Naziwsko, czytelnik.Email, czytelnik.Telefon, czytelnik.Adres };
+            if (dc.DatabaseExists()) List_Users.ItemsSource = users_list;
+
             BookList.Visibility = Visibility.Collapsed;
             USERSLIST.Visibility = Visibility.Visible;
             RentBook.Visibility = Visibility.Collapsed;
@@ -91,7 +100,7 @@ namespace WpfApp1
 
         private void ButtonAddUser_Click(object sender, RoutedEventArgs e)
         {
-            //int last_number_ID = dc.Czytelnik.Last().Pk_Czytelnik_Id;
+            int last_number_ID = dc.Czytelnik.Max(u => u.Pk_Czytelnik_Id) + 1;
 
             string name = Name.Text;
             string surname = Surname.Text;
@@ -114,7 +123,14 @@ namespace WpfApp1
             dc.SubmitChanges();
                                                         //Dodać uzytkownika do bazy (poszczególne informacje w stringach)
 
-            Add_Users.Visibility = Visibility.Collapsed; //powrót do listy użytkowników
+            
+
+            //powrót do listy użytkowników
+            var users_list = from czytelnik in dc.Czytelnik
+                             select new { czytelnik.Imie, czytelnik.Naziwsko, czytelnik.Email, czytelnik.Telefon, czytelnik.Adres };
+            if (dc.DatabaseExists()) List_Users.ItemsSource = users_list;
+
+            Add_Users.Visibility = Visibility.Collapsed;
         }
 
         private void ButtonUser1_Click(object sender, RoutedEventArgs e)
@@ -150,7 +166,7 @@ namespace WpfApp1
 
             dc.SubmitChanges();
 
-            Return_Book.Visibility = Visibility.Collapsed; // zpowrót do listy ksiazek w zwrotach
+             // zpowrót do listy ksiazek w zwrotach
 
             var books_to_return = from wypozyczenie in dc.Wypozyczenie
                                   join ksiazka in dc.Ksiazka on wypozyczenie.Fk_Ksiazka_Id equals ksiazka.Pk_Ksiazka_Id
@@ -160,6 +176,7 @@ namespace WpfApp1
                                   select new { wypozyczenie.Pk_Wypozyczenie_Id, ksiazka.Tytul, autor.Imie, autor.Nazwisko, wypozyczenie.Data_oddania };
 
             if (dc.DatabaseExists()) Return1_Book.ItemsSource = books_to_return;
+            Return_Book.Visibility = Visibility.Collapsed;
         }
 
         private void ButtonBorrow2_Click(object sender, RoutedEventArgs e)
@@ -174,7 +191,45 @@ namespace WpfApp1
 
         private void ButtonBorrowBook_Click(object sender, RoutedEventArgs e)
         {
-            //Po naciśnięciu wypożycz zmiana statusu w bazie danych na wypożyczony ksiazki z listy z indeksem (index_list)
+            //Po naciśnięciu wypożycz zmiana statusu w bazie danych na wypożyczony ksiazki z listy z indeksem (index_list)///////////////////////////////////////////////////////////
+            Ksiazka ksiazka = dc.Ksiazka.SingleOrDefault(x => x.Pk_Ksiazka_Id == index_books);
+            ksiazka.Dostepnosc = "wypozyczona";
+
+
+            int wypozyczenieId;
+            int max_wypozyczenie_id;
+            var isempty = dc.Wypozyczenie.FirstOrDefault();
+            if(isempty == null) { wypozyczenieId = 1; }
+            else
+            {
+                max_wypozyczenie_id = dc.Wypozyczenie.Max(u => u.Pk_Wypozyczenie_Id);
+                wypozyczenieId = max_wypozyczenie_id + 1;
+            }
+
+            
+
+            Wypozyczenie noweWypozyczenie = new Wypozyczenie
+            {
+                Pk_Wypozyczenie_Id = wypozyczenieId,
+                Fk_Ksiazka_Id = index_books,
+                Fk_Czytelnik_Id = index_users,
+                Data_oddania = "2019"
+            };
+            dc.Wypozyczenie.InsertOnSubmit(noweWypozyczenie);
+
+
+            dc.SubmitChanges();
+
+            var books_to_return = from wypozyczenie in dc.Wypozyczenie
+                                  join ksiazka1 in dc.Ksiazka on wypozyczenie.Fk_Ksiazka_Id equals ksiazka1.Pk_Ksiazka_Id
+                                  join czytelnik1 in dc.Czytelnik on wypozyczenie.Fk_Czytelnik_Id equals czytelnik1.Pk_Czytelnik_Id
+                                  join autor in dc.Autor on ksiazka1.Fk_Author_Id equals autor.Pk_Autor_Id
+                                  where wypozyczenie.Fk_Czytelnik_Id == index_users
+                                  select new { wypozyczenie.Pk_Wypozyczenie_Id, ksiazka1.Tytul, autor.Imie, autor.Nazwisko, wypozyczenie.Data_oddania };
+
+            if (dc.DatabaseExists()) Return1_Book.ItemsSource = books_to_return;
+
+
 
             Borrow_Book.Visibility = Visibility.Collapsed; //powrót do listy książek
         }
@@ -222,13 +277,15 @@ namespace WpfApp1
         //Pobranie informacji o indeksie naciśnietej ksiażki w liście ksiażek
         private void List_Book_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-            index_list = List_Book.SelectedIndex; //informacja o naciśniętym indexie w tabeli Listy Książek
+            index_books = List_Book.SelectedIndex + 1; //informacja o naciśniętym indexie w tabeli Listy Książek
+            Ksiazka ksiazka = dc.Ksiazka.SingleOrDefault(x => x.Pk_Ksiazka_Id == index_books);
 
             Borrow_Book.Visibility = Visibility.Visible; //wyświetlenie okna do wypożyczenia
 
-                                                //Sprawdzić dostepność książki
+            //Sprawdzić dostepność książki
+            
 
-            string Bookstatus = "dostepna";     //Bookstatus przyjmuje wartość statusu ksiazki
+            string Bookstatus = ksiazka.Dostepnosc;//Bookstatus przyjmuje wartość statusu ksiazki
             if (Bookstatus == "dostepna")
             {
                 Bookavailable.Visibility = Visibility.Visible;
